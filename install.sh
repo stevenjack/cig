2d38f9a3760f8f34f05bb4e6cbfa7abb389512eb
Steven Jack <stevenmajack@gmail.com>
2015-04-20 07:46:16 +0100

    Change install script to use release

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -L https://s3-eu-west-1.amazonaws.com/cig.steven-jack.co.uk/0.1.0/cig_`uname -s`_`uname -m` > /usr/local/bin/cig
+curl -L https://github.com/stevenjack/cig/releases/download/v0.1.0/cig_`uname -s`_`uname -m` > /usr/local/bin/cig
 chmod +x /usr/local/bin/cig
