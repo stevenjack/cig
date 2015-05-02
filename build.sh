e1495da399b8df90a68f1d6ffe29a32284d14798
Steven Jack <stevenmajack@gmail.com>
2015-05-02 15:15:23 +0100

    Fix build script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $TRAVIS_BRANCH == 'master' ]]
+if [[ $TRAVIS_BRANCH == 'master' ]]; then
   go get github.com/mitchellh/gox
   gox -os="darwin linux windows" -arch="amd64" -build-toolchain
   gox -os="darwin linux windows" -arch="amd64"
