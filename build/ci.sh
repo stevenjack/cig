53ef8e1811b576173f84040d10b79cc7d25480ce
Steven Jack <stevenmajack@gmail.com>
2015-05-03 21:47:45 +0100

    Closes #24 - Only build if branch looks like release tag

--- build/ci.sh
+++ build/ci.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [[ $TRAVIS_BRANCH == 'master' ]]; then
+go test
+
+if [[ $TRAVIS_BRANCH =~ "^v[0-9\.]+" ]]; then
   go get github.com/mitchellh/gox
   gox -os="darwin linux windows" -arch="amd64" -build-toolchain
   gox -os="darwin linux windows" -arch="amd64" -output="{{.Dir}}_{{.OS}}_x86_64"
-else
-  go test
 fi
