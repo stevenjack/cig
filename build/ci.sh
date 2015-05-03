f39a2c3c9013b9e57f850fac973e59a5984100f6
Steven Jack <stevenmajack@gmail.com>
2015-05-03 17:40:50 +0100

    Minor release 0.0.1

--- build/ci.sh
+++ build/ci.sh
@@ -3,7 +3,7 @@
 if [[ $TRAVIS_BRANCH == 'master' ]]; then
   go get github.com/mitchellh/gox
   gox -os="darwin linux windows" -arch="amd64" -build-toolchain
-  gox -os="darwin linux windows" -arch="amd64"
+  gox -os="darwin linux windows" -arch="amd64" -output="{{.Dir}}_{{.OS}}_x86_64"
 else
   go test
 fi
