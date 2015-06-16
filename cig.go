c287a1597be58617d4ef173d3391b789df8b27d9
Steven Jack <stevenmajack@gmail.com>
2015-06-17 00:17:38 +0100

    Patch release 0.1.3

--- cig.go
+++ cig.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stevenjack/cig/output"
 )
 
-const version string = "0.1.2"
+const version string = "0.1.3"
 
 func main() {
 	var output_channel = make(chan output.Payload)
--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -L https://github.com/stevenjack/cig/releases/download/v0.1.2/cig_`uname -s`_x86_64 > /usr/local/bin/cig
+curl -L https://github.com/stevenjack/cig/releases/download/v0.1.3/cig_`uname -s`_x86_64 > /usr/local/bin/cig
 chmod +x /usr/local/bin/cig
