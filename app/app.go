f573be08a7869c1a4e0875834fa23d3469ccdd90
Steven Jack <stevenmajack@gmail.com>
2015-10-04 14:54:41 +0100

    Refactor variable names to be in line with go convention

--- app/app.go
+++ app/app.go
@@ -13,30 +13,30 @@ import (
 	"github.com/stevenjack/cig/repo"
 )
 
-func Handle(repoList map[string]string, projectTypeToCheck string, filter string, output_channel chan output.Payload) {
+func Handle(repoList map[string]string, projectTypeToCheck string, filter string, outputChannel chan output.Payload) {
 	var wg sync.WaitGroup
 
 	for projectType, path := range repoList {
 		if projectTypeToCheck == "" || projectTypeToCheck == projectType {
-			output_channel <- output.Print(fmt.Sprintf("\nChecking '%s' (%s) repos...", projectType, path))
+                        outputChannel <- output.Print(fmt.Sprintf("\nChecking '%s' (%s) repos...", projectType, path))
 			path = resolvePath(path)
 
 			visit := func(visitedPath string, info os.FileInfo, err error) error {
 				if err != nil {
-					output_channel <- output.Error(fmt.Sprintf("- %s", err.Error()))
+                                        outputChannel <- output.Error(fmt.Sprintf("- %s", err.Error()))
 					return nil
 				}
 				matched, _ := regexp.MatchString(filter, visitedPath)
 				if info.IsDir() && (filter == "" || matched) {
 					wg.Add(1)
-					go repo.Check(path, visitedPath, output_channel, &wg)
+                                        go repo.Check(path, visitedPath, outputChannel, &wg)
 				}
 				return nil
 			}
 
 			err := filepath.Walk(path, visit)
 			if err != nil {
-				output_channel <- output.Error(err.Error())
+                                outputChannel <- output.Error(err.Error())
 			}
 		}
 
--- repo/repo.go
+++ repo/repo.go
@@ -12,7 +12,7 @@ import (
 	"github.com/stevenjack/cig/output"
 )
 
-func Check(root string, path string, output_channel chan output.Payload, wg *sync.WaitGroup) {
+func Check(root string, path string, outputChannel chan output.Payload, wg *sync.WaitGroup) {
 	defer wg.Done()
 	exists, err := Exists(filepath.Join(path, ".git"))
 
@@ -21,52 +21,52 @@ func Check(root string, path string, output_channel chan output.Payload, wg *syn
 	}
 
 	if exists {
-		modified_files := exec.Command("git", "status", "--porcelain")
-		modified_files.Dir = path
+                modifiedFiles := exec.Command("git", "status", "--porcelain")
+                modifiedFiles.Dir = path
 
-		count_out, _ := modified_files.Output()
-		modified_lines := strings.Split(string(count_out), "\n")
-		modified := len(modified_lines) - 1
+                countOut, _ := modifiedFiles.Output()
+                modifiedLines := strings.Split(string(countOut), "\n")
+                modified := len(modifiedLines) - 1
 
 		if err != nil {
-			output_channel <- output.FatalError(err.Error())
+                        outputChannel <- output.FatalError(err.Error())
 		}
 
 		changes := []string{}
 
-		if modified > 0 && modified_lines[0] != "" {
+                if modified > 0 && modifiedLines[0] != "" {
 			changes = append(changes, output.ApplyColour(fmt.Sprintf(" M(%d)", modified), "red"))
 		}
 
 		branch := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD")
 		branch.Dir = path
 		bstdout, _ := branch.Output()
-		branch_name := strings.TrimSpace(string(bstdout[:]))
+                branchName := strings.TrimSpace(string(bstdout[:]))
 
-		local := exec.Command("git", "rev-parse", branch_name)
+                local := exec.Command("git", "rev-parse", branchName)
 		local.Dir = path
 		lstdout, _ := local.Output()
-		local_ref := strings.TrimSpace(string(lstdout[:]))
+                localRef := strings.TrimSpace(string(lstdout[:]))
 
-		remote := exec.Command("git", "rev-parse", fmt.Sprintf("origin/%s", branch_name))
+                remote := exec.Command("git", "rev-parse", fmt.Sprintf("origin/%s", branchName))
 		remote.Dir = path
 		rstdout, err := remote.Output()
-		remote_ref := strings.TrimSpace(string(rstdout[:]))
+                remoteRef := strings.TrimSpace(string(rstdout[:]))
 
-		if err == nil && remote_ref != local_ref {
+                if err == nil && remoteRef != localRef {
 			changes = append(changes, output.ApplyColour(" P", "blue"))
 		}
 
 		if len(changes) > 0 {
 			var buffer bytes.Buffer
 
-			repo_name := strings.Replace(path, fmt.Sprintf("%s%s", root, string(os.PathSeparator)), "", -1)
+                        repoName := strings.Replace(path, fmt.Sprintf("%s%s", root, string(os.PathSeparator)), "", -1)
 
-			buffer.WriteString(fmt.Sprintf("- %s (%s)", repo_name, branch_name))
+                        buffer.WriteString(fmt.Sprintf("- %s (%s)", repoName, branchName))
 			for _, change := range changes {
 				buffer.WriteString(change)
 			}
-			output_channel <- output.Print(buffer.String())
+                        outputChannel <- output.Print(buffer.String())
 		}
 
 	}
